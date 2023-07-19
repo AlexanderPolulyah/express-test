caed70791fb4c6dd5e80c3d81e8418a97ad1c2ed
Alexander <alex1.polulyah@gmail.com>
2023-07-19 19:15:59 +0300

    3

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.post('/test', (req, res) => {
-    res.status(200).json('ответ на тест!')
+app.get('/testGet', (req, res) => {
+    res.status(200).json('ответ на тест через GET!')
+})
+app.post('/testPost', (req, res) => {
+    res.status(200).json('ответ на тест через POST!')
 })
 
 app.listen(3000, () => console.log('server started...'))
